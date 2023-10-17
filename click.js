86c4ef932877f8e677daf6f8759e8e3cc16761a8
Yuchen <79828908+Yuchen-ck@users.noreply.github.com>
2023-10-17 12:33:00 +0800

    Update click.js

--- click.js
+++ click.js
@@ -7,6 +7,11 @@ function openGithub() {
     window.open('https://github.com/Yuchen-ck', '_blank');
 }
 
+function openPaper() {
+    window.open('https://ndltd.ncl.edu.tw/cgi-bin/gs32/gsweb.cgi/ccd=ZydVNt/record?r1=1&h1=0', '_blank');
+}
+
+
 function downloadPDF() {
     var link = document.createElement('a');
     link.href = './paper/paper.pdf';
